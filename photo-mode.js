521f5bb9bb934f81ce9229158e1d1d84393a3c96
Philip Wogart <phi@headgear.io>
2019-05-28 22:52:26 +0200

    first commit

---
+++ photo-mode.js
@@ -0,0 +1,46 @@
+AFRAME.registerComponent('photo-mode', {
+  init: function() {
+    const container = document.getElementById('photoModeContainer')
+    const image = document.getElementById('photoModeImage')
+    const shutterButton = document.getElementById('shutterButton')
+    const switchButton = document.getElementById('switchButton')
+    const closeButton = document.getElementById('closeButton')
+
+    // Container starts hidden so it isn't visible when the page is still loading
+    container.style.display = 'block'
+
+    closeButton.addEventListener('click', () => {
+      container.classList.remove('photo')
+    })
+
+    shutterButton.addEventListener('click', () => {
+      // Emit a screenshotrequest to the xrweb component
+      this.el.sceneEl.emit('screenshotrequest')
+
+      // Show the flash while the image is being taken
+      container.classList.add('flash')
+    })
+
+    // switchButton.addEventListener('click', () => {
+    //   // Emit a modelrequest to the xrweb component
+    //   this.el.sceneEl.querySelector('a-entity#carModel').setAttribute('visible', 'true')
+    // })
+
+    this.el.sceneEl.addEventListener('screenshotready', e => {
+      // Hide the flash
+      container.classList.remove('flash')
+
+      // If an error occurs while trying to take the screenshot, e.detail will be empty.
+      // We could either retry or return control to the user
+      if (!e.detail) {
+        return
+      }
+
+      // e.detail is the base64 representation of the JPEG screenshot
+      image.src = 'data:image/jpeg;base64,' + e.detail
+
+      // Show the photo
+      container.classList.add('photo')
+    })
+  }
+})
